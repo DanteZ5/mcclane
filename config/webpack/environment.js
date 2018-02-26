b758ef41c71c37f90e152a83fdccc519dbf6c944
Dante <dplanterose@gmail.com>
2018-02-26 17:21:15 +0100

    Force node version on Heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-// Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
 environment.plugins.set('Provide',
   new webpack.ProvidePlugin({
