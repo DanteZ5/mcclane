a8b211fb6eef554796ac21592c4a9a612dbde6b0
Nass <mezouar.nass@gmail.com>
2018-02-27 12:19:46 +0100

    add validation to template and colevents

--- app/models/colevent.rb
+++ app/models/colevent.rb
@@ -1,4 +1,11 @@
 class Colevent < ApplicationRecord
+  before_save :default_values
   belongs_to :collaborator
   belongs_to :event
+
+  validates :safe, presence :true
 end
+
+def default_values
+    self.safe ||= false
+  end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,8 @@
 class Template < ApplicationRecord
   belongs_to :event
+
+  validates :slot, presence: true
+  validates :content, presence: true
+  validates :order, presence: true
+
 end
