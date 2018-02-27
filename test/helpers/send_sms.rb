0040b8701871ad9438e8b6e5e786d2c3f1d75763
Dante <dplanterose@gmail.com>
2018-02-27 17:52:56 +0100

    gems

---
+++ test/helpers/send_sms.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'pry'
+
+account_sid = ENV["account_sid"] # Your Account SID from www.twilio.com/console
+auth_token = ENV["auth_token"]   # Your Auth Token from www.twilio.com/console
+
+binding.pry
+
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+message = @client.messages.create(
+    body: "Hello from McClane",
+    to: "+33634264547",    # Replace with your phone number
+    from: "+33644603214")  # Replace with your Twilio number
+
+puts message.sid
