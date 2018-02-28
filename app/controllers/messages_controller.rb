dc37db24f858c9b470109da2bffbb1e94963f8dc
Dante <dplanterose@gmail.com>
2018-02-28 17:52:17 +0100

    avancement twilio serveur tout ca

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:listen]
+
+
   def new
     @message = Message.new
     @colevent = Colevent.find(params[:colevent_id])
@@ -8,14 +11,26 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.colevent_id = params[:colevent_id]
+    @message.destination = 'outbound'
     @message.save
-    @message.send_sms
+    @message.send_sms # methode dans model
+  end
+
+  def listen
+    @message = params["Body"]
+    account_sid = ENV["account_sid"] # Your Account SID from www.twilio.com/console
+    auth_token = ENV["auth_token"]   # Your Auth Token from www.twilio.com/console
+    # Initialize Twilio Client
+    # @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    # @call = @client.api.calls('CA42ed11f93dc08b952027ffbc406d0868').fetch
+
   end
 
   private
 
   def message_params
     #white list
-    params.require(:message).permit(:question_content, :status, :answer_content, :answer_time, :phone_number, :colevent_id)
+    params.require(:message).permit(:content, :status, :type, :phone_number, :colevent_id)
   end
 end
