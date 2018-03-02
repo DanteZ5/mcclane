b3766ab35c1314c2b8a05de972eedceecbc56e58
Thibaud Vuitton <vuittont@gmail.com>
2018-03-02 15:05:26 +0100

    send a message to a specific user

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import '../components/select2';
 import '../components/tables'
-import { switcher } from '../components/tabs';
-switcher();
 import { messenger } from '../components/messenger';
 messenger();
+import { switcher } from '../components/tabs';
+switcher();
