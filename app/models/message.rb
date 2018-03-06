f56c513649c9181782ed73135386be6af31df6d6
Dante <dplanterose@gmail.com>
2018-03-06 11:08:56 +0100

    scheduled is operationnal

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,9 +6,24 @@ class Message < ApplicationRecord
    validates :phone_number, presence: true
 
 
-  def send_sms
-    SmsJob.perform_later(self.id)
-  end
+  # def send_sms
+  #   SmsJob.perform_later(self.id)
+  # end
+
+  # def scheduled_sms(timer)
+  #   SmsJob.set(wait: timer.minutes).perform_later(self.id)
+  # end
+
+  # def delete_job
+
+  # end
+
 
   # !!appliquer les regles des que possible
 end
+
+
+
+# FakeJob.set(wait: 1.minute).perform_later
+
+# FakeJob.set(wait_until: Date.tomorrow.noon).perform_later
