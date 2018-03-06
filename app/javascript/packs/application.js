404ce2fb9df3e9c5c2169b508f04f51eada24da1
Maximilien Rufin <maximilien.rufin@gmail.com>
2018-03-06 17:28:00 +0100

    sticky fixed and added tooltip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import '../components/select2';
 import '../components/tables';
 import '../components/sticky';
+import '../components/tooltip';
 import { messenger } from '../components/messenger';
 messenger();
 import { switcher } from '../components/tabs';
