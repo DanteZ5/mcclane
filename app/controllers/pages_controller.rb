957a0a4022be289e2fb1436ddae74421e7829eb4
Dante <dplanterose@gmail.com>
2018-03-08 10:26:56 +0100

    fix bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user! # , only: [:home, :subscription]
+  skip_before_action :authenticate_user!, only: [:home, :subscription, :create, :thanks]
 
   def home
   end
